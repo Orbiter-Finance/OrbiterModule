5fc3e1872298ccfe42d7e174841449d2e192be8d
wanqing <1799476517@qq.com>
2022-01-10 00:29:36 +0800

    1. add dashboard 2. add dependencies: chartjs 3. update MakerHistory.vue

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,6 +5,10 @@ import installElementPlus from './plugins/element'
 import installVxeTale from './plugins/vxe-table'
 import router from './router'
 import store from './store'
+import { Chart, registerables } from 'chart.js'
+
+// Chart register
+Chart.register(...registerables)
 
 const app = createApp(App).use(store).use(router)
 installElementPlus(app)
