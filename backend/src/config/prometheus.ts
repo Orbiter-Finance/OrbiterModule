e4a9e0e1f8ab01f3f83231bd17f7aaa3e9d28564
linkdrone <fengwanqing2014@gmail.com>
2022-02-10 15:22:13 +0800

    fixed bugs

--- backend/src/config/prometheus.ts
+++ backend/src/config/prometheus.ts
@@ -1,3 +1,9 @@
+let envAlertmanager: { api: string } | undefined
+const { PROMETHEUS_ALERTMANAGER } = process.env
+if (PROMETHEUS_ALERTMANAGER) {
+  envAlertmanager = JSON.parse(PROMETHEUS_ALERTMANAGER)
+}
+
 export const alertmanager = {
-  api: 'http://localhost:9093',
+  api: envAlertmanager?.api || `http://localhost:9093`,
 }
