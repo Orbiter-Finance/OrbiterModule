5c910258d44c9c3d77c0fd46be562f63cd341684
linkdrone <fengwanqing2014@gmail.com>
2022-03-24 16:17:18 +0800

    fixed bugs

--- backend/src/util/maker/index.ts
+++ backend/src/util/maker/index.ts
@@ -1257,7 +1257,7 @@ function confirmToLPTransaction(
   accessLogger.info('confirmToLPTransaction =', getTime())
   setTimeout(async () => {
     try {
-      checkLoopringAccountKey(makerAddress, toChainId)
+      await checkLoopringAccountKey(makerAddress, toChainId)
       const GetUserTransferListRequest = {
         accountId: accountInfo.accountId,
         hashes: txID,
