b8c0a53534d423e8a0d86658aab9e3469feb0afe
allenzend <allenzend@sina.com>
2022-04-12 15:59:12 +0800

    nothing

--- backend/src/util/maker/send.ts
+++ backend/src/util/maker/send.ts
@@ -1,4 +1,5 @@
 import { ERC20TokenType, ETHTokenType } from '@imtbl/imx-sdk'
+import BigNumber from "bignumber.js"
 import {
   ChainId, ConnectorNames, ExchangeAPI, generateKeyPair, GlobalAPI, UserAPI,
   VALID_UNTIL
