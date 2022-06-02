41c41c0070fe0def88ad05ac876bcb463ba2e5c8
0xKate <huanhui.boy@gmail.com>
2022-06-02 15:35:11 +0800

    remove test trx

--- backend/src/util/maker/new_maker.ts
+++ backend/src/util/maker/new_maker.ts
@@ -94,42 +94,7 @@ export async function startNewMakerTrxPull() {
     // }
     scanChain.mq.subscribe(`${intranetId}:txlist`, subscribeNewTransaction)
   }
-  subscribeNewTransaction([
-    {
-      chainId: '4',
-      hash: '0x7aef9fc87c0516f6f943544a78a28f5b499853806ea6ac94b663ac5f6c99e041',
-      nonce: 0,
-      blockHash:
-        '0x2aab838c45ffd9ed38dfc1b2515f98d6a586c9f4e56ff78341303c5f8e9cb23d',
-      blockNumber: 10770101,
-      transactionIndex: 30,
-      from: '0xBF8128Fba8E855702Dc692E6cf534F9FBdf465C6',
-      to: '0x0043d60e87c5dd08C86C3123340705a1556C4719',
-      value: new BigNumber('7100000000009514'),
-      symbol: 'ETH',
-      gasPrice: 1100000010,
-      gas: 21000,
-      input: '0x',
-      status: 1,
-      tokenAddress: '0x0000000000000000000000000000000000000000',
-      timestamp: 1653970230,
-      fee: 23100000210000,
-      feeToken: 'ETH',
-      extra: {
-        accessList: [],
-        chainId: '0x4',
-        maxFeePerGas: '1100000010',
-        maxPriorityFeePerGas: '1100000010',
-        r: '0x730cbf5b27c7814ffa19542401f1c5d87a476abb7fd9fcfd152b80dfaaf73b79',
-        s: '0x15b9911a99419e3f58d3d4077a113f2cb9be53dcc21ef1c51d65c7507ddc6e85',
-        type: 2,
-        v: '0x1',
-      },
-      source: 'rpc',
-      confirmations: 5446,
-    },
-  ])
-  // scanChain.run()
+  scanChain.run()
 }
 async function subscribeNewTransaction(newTxList: Array<ITransaction>) {
   // Transaction received
