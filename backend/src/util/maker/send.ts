df66f520de6039362c82f84b234eee071a620462
kakui <huanhui.boy@gmail.com>
2022-11-30 15:34:01 +0800

    SendQueue add checkHealth

--- backend/src/util/maker/send.ts
+++ backend/src/util/maker/send.ts
@@ -26,13 +26,22 @@ import { getTargetMakerPool } from '../../service/maker'
 import { SendQueue } from './send_queue'
 import { StarknetHelp } from '../../service/starknet/helper'
 import { equals, isEmpty } from 'orbiter-chaincore/src/utils/core'
-import { accessLogger, getLoggerService } from '../logger'
+import { accessLogger, errorLogger, getLoggerService } from '../logger'
 import { chains } from 'orbiter-chaincore'
-
+import { doSms } from '../../sms/smsSchinese'
 const PrivateKeyProvider = require('truffle-privatekey-provider')
 
 const nonceDic = {}
-
+const checkHealthByChain: {
+  [key: string]: {
+    smsInterval: number,
+    lastSendSMSTime: number
+  }
+} = {};
+const checkHealthAllChain = {
+  smsInterval: 1000 * 30,
+  lastSendSMSTime: 0
+};
 const getCurrentGasPrices = async (toChain: string, maxGwei = 165) => {
 
   if (toChain === 'mainnet' && !makerConfig[toChain].gasPrice) {
@@ -103,9 +112,31 @@ const getCurrentGasPrices = async (toChain: string, maxGwei = 165) => {
     }
   }
 }
-
 // SendQueue
 const sendQueue = new SendQueue()
+sendQueue.checkHealth((timeout) => {
+  if (Date.now() - checkHealthAllChain.lastSendSMSTime >= checkHealthAllChain.smsInterval) {
+    checkHealthAllChain.lastSendSMSTime = Date.now();
+    try {
+      doSms(`Warning: From last consumption ${timeout / 1000}/s`)
+    } catch (error) {
+      errorLogger.error(
+        `Warning: From last consumption doSMS error `,
+        error
+      )
+    }
+  }
+}, (chainId: number, timeout: number) => {
+  const config = checkHealthByChain[chainId] || {
+    smsInterval: 1000 * 30,
+    lastSendSMSTime: 0
+  };
+  if (Date.now() - config.lastSendSMSTime >= config.smsInterval) {
+    config.lastSendSMSTime = Date.now();
+    doSms(`Warning:chainId ${chainId} From last consumption ${timeout / 1000}/s`);
+  }
+  checkHealthByChain[chainId] = config;
+});
 
 async function sendConsumer(value: any) {
   let {
@@ -1193,7 +1224,7 @@ async function sendConsumer(value: any) {
     try {
       // eip 1559 send
       const config = chains.getChainByInternalId(chainID);
-      if (config.rpc.length<=0) {
+      if (config.rpc.length <= 0) {
         throw new Error('Missing RPC configuration')
       }
       const httpsProvider = new ethers.providers.JsonRpcProvider(config.rpc[0]);
@@ -1203,7 +1234,7 @@ async function sendConsumer(value: any) {
       delete details['gas'];
       let maxPriorityFeePerGas = 1000000000;
       try {
-        if (config.rpc.length>0 && config.rpc[0].includes('alchemyapi')) {
+        if (config.rpc.length > 0 && config.rpc[0].includes('alchemyapi')) {
           const alchemyMaxPriorityFeePerGas = await httpsProvider.send("eth_maxPriorityFeePerGas", []);
           if (Number(alchemyMaxPriorityFeePerGas) > maxPriorityFeePerGas) {
             maxPriorityFeePerGas = alchemyMaxPriorityFeePerGas;
@@ -1212,7 +1243,7 @@ async function sendConsumer(value: any) {
       } catch (error) {
         accessLogger.error('eth_maxPriorityFeePerGas error', error.message);
       }
-     
+
       details['maxPriorityFeePerGas'] = web3.utils.toHex(maxPriorityFeePerGas);
       details['maxFeePerGas'] = web3.utils.toHex(maxPrice * 10 ** 9);
       details['gasLimit'] = web3.utils.toHex(gasLimit);
