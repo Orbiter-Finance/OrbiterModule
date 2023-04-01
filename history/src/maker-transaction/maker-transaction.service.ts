7f7070e5cdb09cf6ade8c2494013a11e42fdf8a7
skywell <719779468@qq.com>
2023-04-01 12:33:43 +0800

    replySender

--- history/src/maker-transaction/maker-transaction.service.ts
+++ history/src/maker-transaction/maker-transaction.service.ts
@@ -25,7 +25,7 @@ export class MakerTransactionService {
 
     let more = ``;
     if (query.makerAddress) {
-      more += `and (m.replySender = '${query.makerAddress}' or t.to = '${query.makerAddress}' or t2.from = '${query.makerAddress}') `
+      more += `and (t.replySender = '${query.makerAddress}' or t.to = '${query.makerAddress}' or t2.from = '${query.makerAddress}') `
     }
     if (query.startTime) {
       more += `and t.timestamp >= '${formateTimestamp(+query.startTime)}' `;
