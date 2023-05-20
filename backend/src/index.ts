5c9c1b72cd5ae69b7eecc83e9c3e271ac542951a
skywell <719779468@qq.com>
2023-05-20 14:28:56 +0800

    docker

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,3 @@
-import { LoggerService } from 'orbiter-chaincore/src/utils';
-import cluster from 'cluster'
 import Koa from 'koa'
 import koaBodyparser from 'koa-bodyparser'
 import cors from 'koa2-cors'
