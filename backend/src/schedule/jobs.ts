03018d7e2f4a9ef7db22850c57ba6a4586a0f76f
skywell <719779468@qq.com>
2023-07-12 18:22:21 +0800

    sn fix

--- backend/src/schedule/jobs.ts
+++ backend/src/schedule/jobs.ts
@@ -313,10 +313,8 @@ export async function batchTxSend(chainIdList = [4, 44]) {
                     const arr: string[] = error.message.split(', got: ');
                     const nonce1 = arr[0].replace(/[^0-9]/g, "");
                     const nonce2 = arr[1].replace(/[^0-9]/g, "");
-                    if (Number(nonce) !== Number(nonce1) && Number(nonce) !== Number(nonce2)) {
-                        accessLogger.error(`starknet sequencer error: ${nonce} != ${nonce1}, ${nonce} != ${nonce2}`);
-                        await starknet.pushTask(queueList);
-                    }
+                    accessLogger.error(`starknet sequencer error: ${nonce} != ${nonce1}, ${nonce} != ${nonce2}`);
+                    await starknet.pushTask(queueList);
                 }
                 await rollback(error, nonce);
                 await sendTxConsumeHandle({
