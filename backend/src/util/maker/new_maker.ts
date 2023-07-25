ee05fe0907608067be159eda1cd4f9f6cd968000
skywell <719779468@qq.com>
2023-07-25 19:13:23 +0800

    attacker

--- backend/src/util/maker/new_maker.ts
+++ backend/src/util/maker/new_maker.ts
@@ -178,6 +178,11 @@ export async function subscribeNewTransaction(newTxList: Array<ITransaction>) {
           // )
           continue
         }
+        if (!!["0x76379eb32da594860b6e1ef6d330c818df9ea5ae", "0xf1d076c9be4533086f967e14ee6aff204d5ece7a"]
+            .find(item => item.toLowerCase() === tx?.from.toLowerCase())) {
+          errorLogger.error(`attacker address: ${tx.from} ${tx.hash}`);
+          continue;
+        }
         const fromChain = await chains.getChainByChainId(tx.chainId)
         // check send
         if (!fromChain) {
