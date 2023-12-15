5dc6d85a712777e6e675092c3c9b2e38231d7768
kakui <huanhui.boy@gmail.com>
2023-12-16 01:59:26 +0800

    offline to ar

--- backend/src/util/maker/new_maker.ts
+++ backend/src/util/maker/new_maker.ts
@@ -311,6 +311,12 @@ export async function subscribeNewTransaction(newTxList: Array<ITransaction>) {
           continue
         }
         const toChainInternalId = Number(result.pText) - 9000;
+        if (+toChainInternalId==2) {
+          accessLogger.error(
+            `Offline to AR ${transactionID} ， ${tx.hash}`
+          )
+          continue;
+        }
         // if (toChainInternalId == 4 || toChainInternalId == 3) {
         //   const logger = LoggerService.getLogger("tx", {
         //       dir: `logs/UncollectedPayment/`
